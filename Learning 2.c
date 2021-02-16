3a66d1b76c59617d2fe4ca28a0e265ffcff3d2ba
Shivam-9360 <70937855+Shivam-9360@users.noreply.github.com>
2021-02-16 17:02:59 +0530

    Learning 2.c
    Menu Driven Program.

---
+++ Learning 2.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <math.h>
+main()
+{
+	int choice;
+	printf("Enter a number between 1 and 5: ");
+	scanf("%d", &choice);
+	
+	switch(choice)
+	{
+		case 1:
+			printf("Food item - Sandwich \nPrice - Rs. 149");
+			break;
+		case 2:
+			printf("Food item - French Fries \nPrice - Rs. 325");
+			break;
+		case 3:
+			printf("Food item - Pasta \nPrice - Rs. 143");
+			break;
+		case 4:
+			printf("Food item - Burger \nPrice - Rs. 165");
+			break;
+		case 5:
+			printf("Food item - Pizza \nPrice - Rs. 200");
+			break;
+		default:
+			printf("Invalid Input");
+	}
+}
+
