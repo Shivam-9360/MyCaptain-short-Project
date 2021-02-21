f6f34982500226ad77bba3b2771a9e6efdd125f0
Shivam-9360 <70937855+Shivam-9360@users.noreply.github.com>
2021-02-21 14:16:41 +0530

    Learning 3.c
    Grading System

--- Learning 2.c
+++ Learning 2.c
@@ -1,30 +1,17 @@
 #include <stdio.h>
 #include <math.h>
-main()
+int main()
 {
-	int choice;
-	printf("Enter a number between 1 and 5: ");
-	scanf("%d", &choice);
-	
-	switch(choice)
-	{
-		case 1:
-			printf("Food item - Sandwich \nPrice - Rs. 149");
-			break;
-		case 2:
-			printf("Food item - French Fries \nPrice - Rs. 325");
-			break;
-		case 3:
-			printf("Food item - Pasta \nPrice - Rs. 143");
-			break;
-		case 4:
-			printf("Food item - Burger \nPrice - Rs. 165");
-			break;
-		case 5:
-			printf("Food item - Pizza \nPrice - Rs. 200");
-			break;
-		default:
-			printf("Invalid Input");
-	}
+    int mks;
+    printf("Enter marks: ");
+    scanf("%d", &mks);
+    
+    if(mks >= 85 && mks <= 100)	printf("Grade A");
+    else if(mks >= 70 && mks <= 84)	printf("Grade B");
+    else if(mks >= 55 && mks <= 69)	printf("Grade C");
+    else if(mks >= 40 && mks <= 54)	printf("Grade D");
+    else printf("Grade F");
+    
+    return 0;
 }
 
