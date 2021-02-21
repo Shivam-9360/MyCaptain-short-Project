6fa650baf2fdd67498a170a300c6428a06a10ba9
Shivam-9360 <70937855+Shivam-9360@users.noreply.github.com>
2021-02-21 15:48:15 +0530

    Learning 4.c
    Pattern

--- Learning 4.c
+++ Learning 4.c
@@ -1,14 +1,19 @@
 #include <stdio.h>
 #include <math.h>
+
 int main()
 {
     int num;
     printf("Enter a number: ");
     scanf("%d", &num);
     
-    int i;
-    for(i=0 ; i<num ; i++)
-    printf("01 ");
+    int i,j;
+    for(i=1 ; i<=num ; i++)
+    {
+    	for(j=1; j<=i ; j++)
+    	printf("01 ");
+    	printf("\n");
+	}
     
     return 0;
 }
