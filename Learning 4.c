fc419e8bdf36b10637f98f00b47285b58c6186ed
Shivam-9360 <70937855+Shivam-9360@users.noreply.github.com>
2021-02-21 14:27:11 +0530

    Learning 4.c
    Loops

---
+++ Learning 4.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <math.h>
+int main()
+{
+    int num;
+    printf("Enter a number: ");
+    scanf("%d", &num);
+    
+    int i;
+    for(i=0 ; i<num ; i++)
+    printf("01 ");
+    
+    return 0;
+}
